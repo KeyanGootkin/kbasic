0128f0f1305a315c13513ed6a346edf8cc7edd65
Keyan <keyan@Mac.lan>
2026-02-22 13:20:39 -1000

    make colored text easier

--- src/kbasic/bar.py
+++ src/kbasic/bar.py
@@ -2,6 +2,7 @@
 # >-|===|>                             Imports                             <|===|-<
 # !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
 from kbasic.typing import Number
+from kbasic.shell import color as color_text
 from contextlib import contextmanager
 import inspect
 from tqdm import tqdm
@@ -17,7 +18,7 @@ from tqdm import tqdm
 # !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
 def bar(
         x: Number, total: Number, 
-        width: int = 20, border="|", block="▉"
+        width: int = 20, border="|", block="▉", color='white'
 ) -> str:
     """create a string representing a progress bar set at 100 * x / total % full.
 
@@ -33,7 +34,7 @@ def bar(
     """
     full = int((x/total) * width // 1)
     empty = width - full
-    bar = border + full*block + empty*" " + border 
+    bar = border + (full-1)*color_text(block, "green") + color_text(block, "yellow") + empty*color_text(block, "black", "faint") + border 
     return bar
 @contextmanager
 def redirect_to_tqdm():
--- src/kbasic/shell.py
+++ src/kbasic/shell.py
@@ -5,7 +5,6 @@ from kbasic.bar import ProgressBar, redirect_to_tqdm
 from kbasic.audio import success
 from kbasic.environment import isAnvil 
 if isAnvil: from kbasic.environment.anvil import anvil_user
-# from kgsim.dhybridr.io import dHybridRinput
 from subprocess import check_output, DEVNULL
 from tqdm import tqdm
 from time import sleep
@@ -20,27 +19,50 @@ import numpy as np
 # !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
 bad = ['\x1b[31m', '\x1b[34m', '\x1b[m']
 ansi = {"BLACK": u"\033[0;30m","RED": u"\x1b[0;31m","GREEN": u"\033[0;32m","YELLOW": u"\033[0;33m","BLUE": u"\033[0;34m","PURPLE": u"\033[0;35m","CYAN": u"\033[0;36m","WHITE": u"\033[0;37m",
-    "FAINT": u"\033[2m","FAINT BLACK": u"\033[2;30m","FAINT RED": u"\x1b[2;31m","FAINT GREEN": u"\033[2;32m","FAINT YELLOW": u"\033[2;33m","FAINT BLUE": u"\033[2;34m","FAINT PURPLE": u"\033[2;35m","FAINT CYAN": u"\033[2;36m","FAINT WHITE": u"\033[2;37m",
+    "BOLD": u"\033[1m","BOLD BLACK": u"\033[1;30m","BOLD RED": u"\x1b[1;31m","BOLD GREEN": u"\033[1;32m","BOLD YELLOW": u"\033[1;33m","BOLD BLUE": u"\033[1;34m","BOLD PURPLE": u"\033[1;35m","BOLD CYAN": u"\033[1;36m","BOLD WHITE": u"\033[1;37m",
     "ITALIC": u"\033[3m","ITALIC BLACK": u"\033[3;30m","ITALIC RED": u"\x1b[3;31m","ITALIC GREEN": u"\033[3;32m","ITALIC YELLOW": u"\033[3;33m","ITALIC BLUE": u"\033[3;34m","ITALIC PURPLE": u"\033[3;35m","ITALIC CYAN": u"\033[3;36m","ITALIC WHITE": u"\033[3;37m",
     "UNDERLINE": u"\033[4m","UNDERLINE BLACK": u"\033[4;30m","UNDERLINE RED": u"\x1b[4;31m","UNDERLINE GREEN": u"\033[4;32m","UNDERLINE YELLOW": u"\033[4;33m","UNDERLINE BLUE": u"\033[4;34m","UNDERLINE PURPLE": u"\033[4;35m","UNDERLINE CYAN": u"\033[4;36m","UNDERLINE WHITE": u"\033[4;37m",
     "BLINK": u"\033[5m","BLINK BLACK": u"\033[5;30m","BLINK RED": u"\x1b[5;31m","BLINK GREEN": u"\033[5;32m","BLINK YELLOW": u"\033[5;33m","BLINK BLUE": u"\033[5;34m","BLINK PURPLE": u"\033[5;35m","BLINK CYAN": u"\033[5;36m","BLINK WHITE": u"\033[5;37m",
     "NEGATIVE": u"\033[7m","BLACK BACKGROUND": u"\033[7;30m","RED BACKGROUND": u"\x1b[7;31m","GREEN BACKGROUND": u"\033[7;32m","YELLOW BACKGROUND": u"\033[7;33m","BLUE BACKGROUND": u"\033[7;34m","PURPLE BACKGROUND": u"\033[7;35m","CYAN BACKGROUND": u"\033[7;36m","WHITE BACKGROUND": u"\033[7;37m",
     "END": u"\033[0m"
 }
-_USERNAME_ = None if not isAnvil else anvil_user
+_USERNAME_ = "x-kgootkin" if not isAnvil else anvil_user
 # !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
 # >-|===|>                            Functions                            <|===|-<
 # !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
-def color(text: str, color: str = 'white', option: str = '') -> str:
+def color_string(text: str, color: str = 'white', option: str = '') -> str:
     match option.lower():
         case '': fmt = ansi[color.upper()] 
-        case 'faint'|'thin': fmt = ansi[f"FAINT {color.upper()}"]
+        case 'bold': fmt = ansi[f"BOLD {color.upper()}"]
         case 'negative'|'background': fmt = ansi[f"{color.upper()} BACKGROUND"]
         case 'italic'|'italics'|'cursive': fmt = ansi[f"ITALIC {color.upper()}"]
         case 'underline': fmt = ansi[f'UNDERLINE {color.upper()}']
         case 'blink'|'blinking': fmt = ansi[f"BLINK {color.upper()}"]
     return fmt + text + ansi['END']
-def warn(text: str): print(color(text, 'yellow'))
+def black(text: str, option: str = '') -> str:
+    return color_string(text, 'black', option)
+def red(text: str, option: str = '') -> str:
+    return color_string(text, 'red', option)
+def green(text: str, option: str = '') -> str:
+    return color_string(text, 'green', option)
+def yellow(text: str, option: str = '') -> str:
+    return color_string(text, 'yellow', option)
+def blue(text: str, option: str = '') -> str:
+    return color_string(text, 'blue', option)
+def purple(text: str, option: str = '') -> str:
+    return color_string(text, 'purple', option)
+def cyan(text: str, option: str = '') -> str:
+    return color_string(text, 'cyan', option)
+def italic(text: str, color: str = 'white') -> str:
+    return color_string(text, color, 'italic')
+def bold(text: str, color: str = 'white') -> str:
+    return color_string(text, color, 'bold')
+def blink(text: str, color: str = 'white') -> str:
+    return color_string(text, color, 'blink')
+def underline(text: str, color: str = 'white') -> str:
+    return color_string(text, color, 'underline')
+def negative(text: str, color: str = 'white') -> str:
+    return color_string(text, color, 'negative')
 def parse_shell_output(output):
         match output:
             case str(): return output 
@@ -52,12 +74,9 @@ def parse_shell_output(output):
                             output[i] = output[i].strip(b)
                 return output
 def system(cmd: str):
-    print(cmd)
     command = cmd.split(' ') if type(cmd)==str else cmd
-    print(command)
     for i in range(len(command)-1):
         if command[i].startswith('"'): 
-            print(command[i])
             start_quote = i 
             end_quote = i+1
             while not command[end_quote].endswith('"'): end_quote+=1
@@ -65,8 +84,8 @@ def system(cmd: str):
             for j in range(start_quote+1, end_quote+1): del command[j]
     output = parse_shell_output(check_output(command, stderr=DEVNULL).decode().splitlines())
     return output
-def anvil(cmd: str):
-    output = parse_shell_output(check_output(['ssh', 'x-kgootkin@anvil.rcac.purdue.edu', *cmd.split(' ')], stderr=DEVNULL).decode().splitlines())
+def anvil(cmd: str, username=_USERNAME_):
+    output = parse_shell_output(check_output(['ssh', f'{username}@anvil.rcac.purdue.edu', *cmd.split(' ')], stderr=DEVNULL).decode().splitlines())
     return output
 async def anvil_async(cmd: str): asyncio.to_thread(anvil, cmd)
 def anvil_queue(username=_USERNAME_): return anvil(f"squeue -u {username}")
